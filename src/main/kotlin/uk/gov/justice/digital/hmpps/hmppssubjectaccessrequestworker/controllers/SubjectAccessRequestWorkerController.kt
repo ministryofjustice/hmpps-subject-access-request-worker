3d9475e9b1320a1f5aeebda0dbfa2a06d5649fea
HorstmannA <anna.horstmann@madetech.com>
2024-02-06 07:43:17 +0000

    Add structure for worker.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/controllers/SubjectAccessRequestWorkerController.kt
@@ -0,0 +1,25 @@
+@RestController
+class SubjectAccessRequestWorkerController() {
+
+//  fun run(): {
+//    chosenSAR: SubjectAccessRequest = pollForNewSubjectAccessRequests()
+//    patchResponse = patch('/api-endpoint?id=' + chosenSAR.id.toString())
+//    if (response == 200) {
+//      doReport()
+//      patch('/api-endpoint?status=completed')
+//    }
+//  }
+//  fun pollForNewSubjectAccessRequests(): SubjectAccessRequest {
+//    var response = []
+//    do {
+//      response = get('/api-endpoint?unclaimed')
+//      time.wait(10 seconds)
+//    } while (response == [])
+//    return response
+//  }
+//
+//  fun doReport() {
+//    ...
+//  }
+
+}
