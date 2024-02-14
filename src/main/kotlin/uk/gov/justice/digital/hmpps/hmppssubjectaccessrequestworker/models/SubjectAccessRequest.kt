6d1d90c8ed8d7c23b372df723b23a5eac503458b
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-02-14 12:30:28 +0000

    Poller (#7)
    * Add structure for worker.
    
    * Add some functionality to poll for SARs.
    
    * Test services and add polling functionality.
    
    * Add auth config to docker-compose.yml.
    
    * Add continued polling and token method.
    
    * Add run-once functionality for tests.
    
    * Increase polling interval to 10 seconds.
    
    * Use variables instead of hard-coding urls.
    
    ---------
    
    Co-authored-by: AprilMD <95350189+AprilMD@users.noreply.github.com>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/models/SubjectAccessRequest.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models
+
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+enum class Status {
+  Pending,
+  Completed,
+}
+
+@Entity
+data class SubjectAccessRequest(
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  val id: Int? = null,
+  @Enumerated(EnumType.STRING)
+  val status: Status = Status.Pending,
+  val dateFrom: LocalDate? = null,
+  val dateTo: LocalDate? = null,
+  val sarCaseReferenceNumber: String = "",
+  val services: String = "",
+  val nomisId: String? = null,
+  val ndeliusCaseReferenceId: String? = null,
+  val requestedBy: String = "",
+  val requestDateTime: LocalDateTime = LocalDateTime.now(),
+  val claimDateTime: LocalDateTime? = null,
+  val claimAttempts: Int = 0,
+  val objectUrl: String? = null,
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/controllers/SubjectAccessRequestWorkerControllerTest.kt
@@ -0,0 +1,1 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.controllers
