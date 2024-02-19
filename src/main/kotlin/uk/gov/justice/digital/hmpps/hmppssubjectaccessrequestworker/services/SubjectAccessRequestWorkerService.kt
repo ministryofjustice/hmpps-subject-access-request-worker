e71d5d2b3fd8a8cc7449e397e2fc5ab51277d78f
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-02-19 15:59:10 +0000

    Poller mods (#15)
    * Split startPolling into two functions.
    
    * Add runTest from kotlin coroutines to test suspend functions.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/SubjectAccessRequestWorkerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/SubjectAccessRequestWorkerService.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.services
 
+import kotlinx.coroutines.delay
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatusCode
@@ -7,17 +8,20 @@ import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.gateways.SubjectAccessRequestGateway
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.SubjectAccessRequest
-import java.time.Duration
 
 @Service
 class SubjectAccessRequestWorkerService(
   @Autowired val sarGateway: SubjectAccessRequestGateway,
-  @Value("\${services.poller.run-once}")
-  private val runOnce: String? = "false",
   @Value("\${services.sar-api.base-url}")
   private val sarUrl: String,
 ) {
-  fun startPolling() {
+  suspend fun startPolling() {
+    while (true) {
+      doPoll()
+    }
+  }
+
+  suspend fun doPoll() {
     val webClient = sarGateway.getClient(sarUrl)
     val token = sarGateway.getClientTokenFromHmppsAuth()
     val chosenSAR = this.pollForNewSubjectAccessRequests(webClient, token)
@@ -26,19 +30,13 @@ class SubjectAccessRequestWorkerService(
       doReport(chosenSAR)
       sarGateway.complete(webClient, chosenSAR, token)
     }
-
-    if (runOnce == "true") {
-      return
-    } else {
-      startPolling()
-    }
   }
 
-  fun pollForNewSubjectAccessRequests(client: WebClient, token: String): SubjectAccessRequest {
+  suspend fun pollForNewSubjectAccessRequests(client: WebClient, token: String): SubjectAccessRequest {
     var response: Array<SubjectAccessRequest>? = emptyArray()
 
     while (response.isNullOrEmpty()) {
-      Thread.sleep(Duration.ofSeconds(10))
+      delay(10)
       response = sarGateway.getUnclaimed(client, token)
     }
     return response.first()
