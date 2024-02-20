dfb7039b8b45e978d9cd27ed52e656f6fd7fd18d
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-02-20 15:52:56 +0000

    Add document storage service integration to the worker. (#9)
    * Add document storage service integration to the worker.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/SubjectAccessRequestWorkerService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/SubjectAccessRequestWorkerService.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.gateways.DocumentStorageGateway
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.gateways.SubjectAccessRequestGateway
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.SubjectAccessRequest
 
@@ -15,6 +16,7 @@ const val POLL_DELAY: Long = 10000
 @Service
 class SubjectAccessRequestWorkerService(
   @Autowired val sarGateway: SubjectAccessRequestGateway,
+  @Autowired val documentStorageGateway: DocumentStorageGateway,
   @Value("\${services.sar-api.base-url}")
   private val sarUrl: String,
 ) {
@@ -53,5 +55,10 @@ class SubjectAccessRequestWorkerService(
 
   fun doReport(sar: SubjectAccessRequest) {
     log.info("Would do report")
+  }
+
+  fun storeSubjectAccessRequestDocument(sarId: Int, docBody: String): String {
+    val idsForReference = documentStorageGateway.storeDocument(sarId, docBody)
+    return idsForReference
   }
 }
