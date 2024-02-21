75b0ef81508ebde98bab1cf8fd4fd420a3fdc7f8
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-02-21 12:47:07 +0000

    Add ability to retrieve data from upstream apis (#13)
    * Add generic upstream API gateway
    
    * Poller (#7)
    
    * Add argument handling logic to SAR data service
    
    * Add get SAR data logic to SAR Worker Service
    
    * Add error handling
    
    ---------
    
    Co-authored-by: Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/gateways/GenericHmppsApiGatewayTest.kt
@@ -0,0 +1,77 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.gateways
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.mockito.Mockito
+import org.mockito.internal.verification.VerificationModeFactory
+import org.mockito.kotlin.verify
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.mockservers.ComplexityOfNeedMockServer
+
+@ActiveProfiles("test")
+@ContextConfiguration(
+  initializers = [ConfigDataApplicationContextInitializer::class],
+  classes = [(GenericHmppsApiGateway::class)],
+)
+class GenericHmppsApiGatewayTest(
+  genericHmppsApiGateway: GenericHmppsApiGateway,
+  @MockBean val mockHmppsAuthGateway: HmppsAuthGateway,
+) : DescribeSpec(
+  {
+    val complexityOfNeedMockServer = ComplexityOfNeedMockServer()
+
+    Mockito.`when`(mockHmppsAuthGateway.getClientToken()).thenReturn("mock-bearer-token")
+
+    beforeEach {
+      complexityOfNeedMockServer.start()
+
+      complexityOfNeedMockServer.stubGetSubjectAccessRequestData()
+    }
+
+    afterTest {
+      complexityOfNeedMockServer.stop()
+    }
+
+    describe("getSarData") {
+
+      it("authenticates using HMPPS Auth with credentials") {
+        val response = genericHmppsApiGateway.getSarData(
+          "http://localhost:4000",
+          "validPrn",
+        )
+
+        verify(mockHmppsAuthGateway, VerificationModeFactory.times(1)).getClientToken()
+      }
+
+      it("retrieves data from the upstream service") {
+        val response = genericHmppsApiGateway.getSarData(
+          "http://localhost:4000",
+          "validPrn",
+        )
+        println(response)
+
+        response.shouldBe(
+          mapOf(
+            "content" to mapOf("additionalProp1" to emptyMap<Any, Any>()),
+          ),
+        )
+      }
+
+      it("returns an error if unable to get a response") {
+
+        val exception = shouldThrow<RuntimeException> {
+          genericHmppsApiGateway.getSarData(
+            "http://localhost:4000",
+            "personNotFoundInSystem",
+          )
+        }
+
+        exception.message.shouldBe("Connection to http://localhost:4000 failed.")
+      }
+    }
+  },
+)
