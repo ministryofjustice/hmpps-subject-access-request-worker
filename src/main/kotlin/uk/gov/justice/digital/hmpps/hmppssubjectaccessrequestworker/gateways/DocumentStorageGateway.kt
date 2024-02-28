0b1a82b88b96a07934d869320bb43b5a4358a02c
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-02-28 10:46:27 +0000

    Add logging for debugging. (#24)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/gateways/DocumentStorageGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/gateways/DocumentStorageGateway.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.gateways
 
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
@@ -13,15 +14,18 @@ class DocumentStorageGateway(
   @Value("\${services.document-storage.base-url}") hmppsDocumentApiUrl: String,
 ) {
   private val webClient: WebClient = WebClient.builder().baseUrl(hmppsDocumentApiUrl).build()
+  private val log = LoggerFactory.getLogger(this::class.java)
 
-  fun storeDocument(documentId: Int, documentBody: String, uuid: String? = UUID.randomUUID().toString()): String {
+  fun storeDocument(documentId: Int, documentBody: String, uuid: String? = UUID.randomUUID().toString()): String? {
+    log.info("Storing document..")
     val token = hmppsAuthGateway.getClientToken()
-    webClient.post().uri("/documents/SUBJECT_ACCESS_REQUEST_REPORT" + { uuid })
+    val response = webClient.post().uri("/documents/SUBJECT_ACCESS_REQUEST_REPORT" + { uuid })
       .header("Authorization", "Bearer $token")
       .header("Service-Name", "DPS-Subject-Access-Requests")
       .bodyValue(documentBody)
       .retrieve().bodyToMono(String::class.java).block()
-    return documentId.toString() + uuid
+    return response
+    // return documentId.toString() + uuid
   }
 
   fun retrieveDocument(documentId: UUID): JSONObject? {
