9eb993b000f851971db7a7009694d8e9264d6317
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-03-08 16:24:45 +0000

    PDF header (#49)
    * Use paragraph to add sections.
    
    * Don't need to handle case of completely empty response - will always contain service headers and 'No Content' as a minimum.
    
    * Remove try-catch from gateway.
    
    * Removed test for no data as it will not happen.
    
    * Restructure the content from services.
    
    * Set margins.
    
    * Check if response exists.
    
    * Format content if it can be mapped.
    
    * Lint.
    
    * Add content check in PDF writing test.
    
    * Add pdf service.
    
    * Add header to PDF.
    
    * Make CustomHeader a model.
    
    * Lint.
    
    * Refactor to remove data changes.
    
    * Remove changes from other PR.
    
    * Add iText doc link to READEME.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/models/CustomHeader.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models
+
+import com.itextpdf.text.Document
+import com.itextpdf.text.Element
+import com.itextpdf.text.Font
+import com.itextpdf.text.Phrase
+import com.itextpdf.text.pdf.ColumnText
+import com.itextpdf.text.pdf.PdfPageEventHelper
+import com.itextpdf.text.pdf.PdfWriter
+
+class CustomHeader(private val nID: String, private val sarID: String) : PdfPageEventHelper() {
+  private var font = Font(Font.FontFamily.COURIER, 10f)
+
+  override fun onEndPage(writer: PdfWriter, document: Document) {
+    val cb = writer.directContent
+    val nIdHeader = Phrase(nID, font)
+    ColumnText.showTextAligned(
+      cb,
+      Element.ALIGN_LEFT,
+      nIdHeader,
+      document.leftMargin(),
+      document.top() + 40,
+      0f,
+    )
+    val sarIdHeader = Phrase("CASE REFERENCE: $sarID", font)
+    ColumnText.showTextAligned(
+      cb,
+      Element.ALIGN_RIGHT,
+      sarIdHeader,
+      document.right(),
+      document.top() + 40,
+      0f,
+    )
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/PdfService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/PdfService.kt
@@ -1,12 +1,22 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.services
 
 import com.itextpdf.text.Document
+import com.itextpdf.text.pdf.PdfPageEventHelper
 import com.itextpdf.text.pdf.PdfWriter
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.CustomHeader
 import java.io.ByteArrayOutputStream
 
 class PdfService {
-  fun getPdfWriter(document: Document, stream: ByteArrayOutputStream): Int {
-    PdfWriter.getInstance(document, stream)
+  fun getPdfWriter(document: Document, stream: ByteArrayOutputStream): PdfWriter {
+    return PdfWriter.getInstance(document, stream)
+  }
+
+  fun getCustomHeader(nID: String, sarID: String): CustomHeader {
+    return CustomHeader(nID, sarID)
+  }
+
+  fun setEvent(writer: PdfWriter, event: PdfPageEventHelper): Int {
+    writer.pageEvent = event
     return 0
   }
 }
