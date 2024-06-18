078f78d3918388369f9a86a9f9d507f002812abe
HorstmannA <anna.horstmann@madetech.com>
2024-06-18 12:46:47 +0100

    First attempt at Mustache.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/Mustache.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker
+
+class Mustache {
+// var mf: MustacheFactory = DefaultMustacheFactory()
+// var m: Mustache = mf.compile("todo.mustache")
+}
