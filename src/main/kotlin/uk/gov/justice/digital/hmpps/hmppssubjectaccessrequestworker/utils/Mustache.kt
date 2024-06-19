7fb650ed8b4f6bc4a5cd3337bdcd5bd9f75fdbb4
HorstmannA <anna.horstmann@madetech.com>
2024-06-19 09:13:48 +0100

    Revert "Add example data and table."
    This reverts commit 5f541d0164fea00189d0de9a980968f294c93918.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/Mustache.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/Mustache.kt
@@ -13,30 +13,14 @@ class Mustache {
     return 0
   }
 
-  fun execute(dataObject: Any): Int {
-
+  fun execute(): Int {
+    val item = Item("Item 1", "02/02/02", "This is the first item.")
     val mf = DefaultMustacheFactory()
     val mustache = mf.compile(mf.getReader("template.mustache"), "hello")
-    mustache.execute(PrintWriter("test.mustache"), dataObject).flush()
+    mustache.execute(PrintWriter("test.mustache"), item).flush()
     return 0
   }
 
 }
 
 data class Item(val title: String, val createdOn: String, val text: String)
-data class Keyworker(
-  val offenderKeyworkerId: String?,
-  val offenderNo: String?,
-  val staffId: String?,
-  val assignedDateTime: String?,
-  val active: String?,
-  val allocationReason: String?,
-  val allocationType: String?,
-  val userId: String?,
-  val prisonId: String?,
-  val expiryDateTime: String?,
-  val deallocationReason: String?,
-  val creationDateTime: String?,
-  val createUserId: String?,
-  val modifyDateTime: String?,
-  val modifyUserId: String?,)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/MustacheTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/MustacheTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.utils
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -17,18 +16,7 @@ class MustacheTest : IntegrationTestBase() {
 
   @Test
   fun `Template executes`() {
-    val testItem = Item("Item 1", "02/02/02", "This is the first item.")
-    val response = Mustache().execute(testItem)
-    Assertions.assertThat(response).isEqualTo(0)
-  }
-
-  @Test
-  fun `keyworker executes`() {
-    val dummyData = """{"offenderKeyworkerId":23264,"offenderNo":"A8610DY","staffId":485588,"assignedDateTime":"2022-07-06T14:42:50.621973","active":true,"allocationReason":"AUTO","allocationType":"A","userId":"TWRIGHT","prisonId":"MDI","expiryDateTime":null,"deallocationReason":null,"creationDateTime":"2022-07-06T14:42:50.625039","createUserId":"TWRIGHT","modifyDateTime":"2022-07-06T14:42:50.625039","modifyUserId":"TWRIGHT"}"""
-    val mapper = jacksonObjectMapper()
-    val keyworker = mapper.readValue(dummyData, Keyworker::class.java)
-    println(keyworker.offenderKeyworkerId)
-    val response = Mustache().execute(keyworker)
+    val response = Mustache().execute()
     Assertions.assertThat(response).isEqualTo(0)
   }
 }
