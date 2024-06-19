2dfe3e64ff82c9c560ef2041045a1eb68bed6d6d
HorstmannA <anna.horstmann@madetech.com>
2024-06-19 11:45:25 +0100

    Add simple mustache mockup.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/Mustache.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.utils
+import com.github.mustachejava.DefaultMustacheFactory
+import com.github.mustachejava.Mustache
+import com.github.mustachejava.MustacheFactory
+import java.io.PrintWriter
+
+class Mustache {
+
+  fun compile(file: String): Int {
+    val mf: MustacheFactory = DefaultMustacheFactory()
+    val m: Mustache? = mf.compile(file)
+    println(m)
+    return 0
+  }
+
+
+  fun execute(dataObject: Any): Int {
+
+    val mf = DefaultMustacheFactory()
+    val mustache = mf.compile(mf.getReader("template.mustache"), "hello")
+    mustache.execute(PrintWriter("test.mustache"), dataObject).flush()
+    return 0
+  }
+}
+
+data class Item(val title: String, val createdOn: String, val text: String)
+data class Keyworker(
+  val offenderKeyworkerId: String?,
+  val offenderNo: String?,
+  val staffId: String?,
+  val assignedDateTime: String?,
+  val active: String?,
+  val allocationReason: String?,
+  val allocationType: String?,
+  val userId: String?,
+  val prisonId: String?,
+  val expiryDateTime: String?,
+  val deallocationReason: String?,
+  val creationDateTime: String?,
+  val createUserId: String?,
+  val modifyDateTime: String?,
+  val modifyUserId: String?,
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/MustacheTest.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.utils
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.integration.IntegrationTestBase
+
+class MustacheTest : IntegrationTestBase() {
+
+    @Test
+    fun `Mustache compiles and returns 0`() {
+        val response = Mustache().compile("template.mustache")
+        Assertions.assertThat(response).isEqualTo(0)
+        assertThrows<Exception> { Mustache().compile("notemplate.mustache") }
+    }
+
+    @Test
+    fun `Template executes`() {
+        val testItem = Item("Item 1", "02/02/02", "This is the first item.")
+        val response = Mustache().execute(testItem)
+        Assertions.assertThat(response).isEqualTo(0)
+    }
+
+
+    @Test
+    fun `keyworker executes`() {
+        val dummyData = """{"offenderKeyworkerId":23264,"offenderNo":"A8610DY","staffId":485588,"assignedDateTime":"2022-07-06T14:42:50.621973","active":true,"allocationReason":"AUTO","allocationType":"A","userId":"TWRIGHT","prisonId":"MDI","expiryDateTime":null,"deallocationReason":null,"creationDateTime":"2022-07-06T14:42:50.625039","createUserId":"TWRIGHT","modifyDateTime":"2022-07-06T14:42:50.625039","modifyUserId":"TWRIGHT"}"""
+        val mapper = jacksonObjectMapper()
+        val keyworker = mapper.readValue(dummyData, Keyworker::class.java)
+        println(keyworker.offenderKeyworkerId)
+        val response = Mustache().execute(keyworker)
+        Assertions.assertThat(response).isEqualTo(0)
+    }
+}
