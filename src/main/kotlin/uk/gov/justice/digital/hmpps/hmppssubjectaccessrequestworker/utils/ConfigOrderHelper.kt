3a1b31329094940c2cb915619c0a49a9d76f5491
HorstmannA <anna.horstmann@madetech.com>
2024-07-19 15:29:20 +0100

    Refactor ConfigOrderHelper files.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/ConfigOrderHelper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/ConfigOrderHelper.kt
@@ -4,27 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import org.springframework.stereotype.Component
 import org.yaml.snakeyaml.LoaderOptions
-import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.DpsService
 import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.DpsServices
 import java.io.File
 
 @Component
 class ConfigOrderHelper {
-
-  fun createOrderedServiceUrlList(configuredOrderedUrlList: List<String>, unorderedSelectedUrlList: MutableList<String>): MutableList<String> {
-    val orderedSelectedUrlList = mutableListOf<String>()
-
-    configuredOrderedUrlList.forEach {
-      if (unorderedSelectedUrlList.contains(it)) {
-        orderedSelectedUrlList.add(it)
-        unorderedSelectedUrlList.removeAt(unorderedSelectedUrlList.indexOf(it))
-      }
-    }
-
-    orderedSelectedUrlList.addAll(unorderedSelectedUrlList)
-
-    return orderedSelectedUrlList
-  }
 
   fun extractServicesConfig(configFilename: String): DpsServices? {
     val loaderOptions = LoaderOptions()
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/ConfigOrderHelperTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/ConfigOrderHelperTest.kt
@@ -11,54 +11,7 @@ class ConfigOrderHelperTest {
   val configOrderHelper = ConfigOrderHelper()
 
   @Nested
-  inner class CreateOrderedServiceUrlList {
-    val orderedUrlList = listOf("test1.com", "test2.com")
-    val sarUrlList = mutableListOf("test2.com", "test1.com")
-
-    @Test
-    fun `createOrderedServiceUrlList returns a list`() = runTest {
-      val orderedSarUrlList = configOrderHelper.createOrderedServiceUrlList(orderedUrlList, sarUrlList)
-
-      Assertions.assertThat(orderedSarUrlList).isInstanceOf(List::class.java)
-    }
-
-    @Test
-    fun `createOrderedServiceUrlList puts the SAR URL list into the order of the ordered URL list`() = runTest {
-      val expectedOrderedSarUrlList = listOf("test1.com", "test2.com")
-
-      val orderedSarUrlList = configOrderHelper.createOrderedServiceUrlList(orderedUrlList, sarUrlList)
-
-      Assertions.assertThat(orderedSarUrlList).isEqualTo(expectedOrderedSarUrlList)
-    }
-
-    @Test
-    fun `createOrderedServiceUrlList adds SAR URLs that do not appear in the ordered URL list onto the end of the orderedSarUrlList`() = runTest {
-      val sarUrlList = mutableListOf("test2.com", "test1.com", "newly-added-service.com")
-      val expectedOrderedSarUrlList = listOf("test1.com", "test2.com", "newly-added-service.com")
-
-      val orderedSarUrlList = configOrderHelper.createOrderedServiceUrlList(orderedUrlList, sarUrlList)
-
-      Assertions.assertThat(orderedSarUrlList).isEqualTo(expectedOrderedSarUrlList)
-    }
-  }
-
-  @Nested
   inner class ExtractServicesConfig {
-
-//    @Test
-//    fun `extractServicesConfig reads config from a file`() = runTest {
-//      val tmpFile = File("test1.txt")
-//      tmpFile.appendText("test1.com\n")
-//      tmpFile.appendText("test2.com\n")
-//      tmpFile.appendText("newly-added-service.com\n")
-//      val expectedConfigString =
-//        "test1.com\ntest2.com\nnewly-added-service.com\n"
-//
-//      val orderedSarUrlList = configOrderHelper.extractServicesConfig("test1.txt")
-//
-//      Assertions.assertThat(orderedSarUrlList).isEqualTo(expectedConfigString)
-//      tmpFile.delete()
-//    }
 
     @Test
     fun `extractServicesConfig reads config from a yaml file and creates a list of service details`() = runTest {
