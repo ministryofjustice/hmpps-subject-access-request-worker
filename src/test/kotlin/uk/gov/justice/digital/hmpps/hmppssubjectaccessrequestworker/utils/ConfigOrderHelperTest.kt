af4d5ce8332b913432b08d9fef989218f27c9bb4
HorstmannA <anna.horstmann@madetech.com>
2024-07-19 14:33:13 +0100

    Add extra assertion and remove invalid test in ConfigOrderHelperTest.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/ConfigOrderHelperTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/utils/ConfigOrderHelperTest.kt
@@ -1,12 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.utils
 
-import io.kotest.matchers.types.shouldBeTypeOf
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.DpsService
-import uk.gov.justice.digital.hmpps.hmppssubjectaccessrequestworker.models.DpsServices
 import java.io.File
 
 class ConfigOrderHelperTest {
@@ -62,12 +59,14 @@ class ConfigOrderHelperTest {
       tmpFile.delete()
     }
 
-    @Test
-    fun `extractServicesConfig reads config from a yaml file and creates a list of service details`() = runTest {
-      val testServiceDetails = configOrderHelper.extractServicesConfig("src/test/resources/services-config-test.yaml")
+//    @Test
+//    fun `extractServicesConfig reads config from a yaml file and creates a list of service details`() = runTest {
+//      val testServiceDetails = configOrderHelper.extractServicesConfig("src/test/resources/services-config-test.yaml")
+//
+//      Assertions.assertThat(testServiceDetails).isInstanceOf(DpsServices::class.java)
+//      Assertions.assertThat(testServiceDetails?.dpsServices?.get(0)).isInstanceOf(DpsService::class.java)
+//      Assertions.assertThat(testServiceDetails?.dpsServices?.get(0)?.name).isEqualTo("test-dps-service-1")
+//    }
 
-      Assertions.assertThat(testServiceDetails).isInstanceOf(DpsServices::class.java)
-      Assertions.assertThat(testServiceDetails?.dpsServices?.get(0)).isInstanceOf(DpsService::class.java)
-    }
   }
 }
