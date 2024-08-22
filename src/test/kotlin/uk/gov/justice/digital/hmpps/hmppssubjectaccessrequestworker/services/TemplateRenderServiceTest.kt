cfb4de601cc3f9e4912da7565b02f562c7ff624d
HorstmannA <anna.horstmann@digital.justice.gov.uk>
2024-08-22 11:32:22 +0100

    Start adding template for adjudications.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/TemplateRenderServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/TemplateRenderServiceTest.kt
@@ -211,7 +211,7 @@ class TemplateRenderServiceTest : DescribeSpec(
     }
 
     describe("incentivesTemplate") {
-      it("renders a template given a incentives template") {
+      it("renders a template given an incentives template") {
         val templateRenderService = TemplateRenderService()
         val testServiceData: ArrayList<Any> = arrayListOf(
           mapOf(
@@ -230,6 +230,171 @@ class TemplateRenderServiceTest : DescribeSpec(
           ),
         )
         val renderedStyleTemplate = templateRenderService.renderTemplate("hmpps-incentives-api", testServiceData)
+        renderedStyleTemplate.shouldNotBeNull()
+        renderedStyleTemplate.shouldContain("<style>")
+        renderedStyleTemplate.shouldContain("</style>")
+        renderedStyleTemplate.shouldContain("<td>Booking ID</td><td>1208204</td>")
+        renderedStyleTemplate.shouldContain("<td>Next review date</td><td>03 December 2019</td>")
+        renderedStyleTemplate.shouldContain("<td>Review time</td><td>03 July 2023, 9:14:25 pm</td>")
+        renderedStyleTemplate.shouldContain("<td>Current</td><td>true</td>")
+      }
+    }
+
+    describe("adjudicationsTemplate") {
+      it("renders a template given an adjudications template") {
+        val templateRenderService = TemplateRenderService()
+        val testServiceData: ArrayList<Any> =
+          arrayListOf(mapOf(
+            "chargeNumber" to "1525733",
+            "prisonerNumber" to "A3863DZ",
+            "gender" to "FEMALE",
+            "incidentDetails" to mapOf(
+            "locationId" to 26149,
+            "dateTimeOfIncident" to "2023-06-08T12:00:00",
+            "dateTimeOfDiscovery" to "2023-06-08T12:00:00",
+            "handoverDeadline" to "2023-06-10T12:00:00",
+            ),
+            "isYouthOffender" to false,
+            "incidentRole" to mapOf(
+            "roleCode" to "25c",
+            "offenceRule" to mapOf(
+            "paragraphNumber" to "25(c)",
+            "paragraphDescription" to "Assists another prisoner to commit, or to attempt to commit, any of the foregoing offences:"
+            ),
+            "associatedPrisonersNumber" to "A3864DZ",
+            ),
+            "offenceDetails" to mapOf(
+            "offenceCode" to 16001,
+            "offenceRule" to mapOf(
+            "paragraphNumber" to "16",
+            "paragraphDescription" to "Intentionally or recklessly sets fire to any part of a prison or any other property, whether or not her own",
+            "nomisCode" to "51:16",
+            "withOthersNomisCode" to "51:25C"
+            ),
+            "protectedCharacteristics" to mapOf(
+              "id" to 247,
+              "characteristic" to "AGE",
+            )
+          ),
+            "incidentStatement" to mapOf(
+            "statement" to "Vera incited Brian Duckworth to set fire to a lamp\r\ndamages - the lamp\r\nevidence includes something in a bag with a reference number of 1234\r\nwitnessed by amarktest",
+            "completed" to true
+            ),
+            "createdByUserId" to "LBENNETT_GEN",
+            "createdDateTime" to "2023-06-08T14:17:20.831884",
+            "status" to "CHARGE_PROVED",
+            "reviewedByUserId" to "AMARKE_GEN",
+            "statusReason" to "",
+            "statusDetails" to "",
+            "damages" to arrayListOf(
+              mapOf(
+              "code" to "ELECTRICAL_REPAIR",
+              "details" to "mend a lamp",
+              "reporter" to "LBENNETT_GEN"
+              )
+            ),
+            "evidence" to arrayListOf(
+              mapOf(
+              "code" to "BAGGED_AND_TAGGED",
+              "identifier" to "1234",
+              "details" to "evidence in a bag with a reference number",
+              "reporter" to "LBENNETT_GEN")
+            ),
+            "witnesses" to arrayListOf(
+              mapOf(
+              "code" to "OFFICER",
+              "firstName" to "Andrew",
+              "lastName" to "Marke",
+              "reporter" to "LBENNETT_GEN"
+              )
+            ),
+            "hearings" to arrayListOf(
+              "id" to 467,
+              "locationId" to 775,
+              "dateTimeOfHearing" to "2023-06-08T14:25:00",
+              "oicHearingType" to "INAD_ADULT",
+              "outcome" to mapOf(
+              "id" to 534,
+              "adjudicator" to "James Warburton",
+              "code" to "COMPLETE",
+              "plea" to "GUILTY"
+              ),
+              "agencyId" to "MDI"
+            )
+          ),
+            "disIssueHistory" to arrayListOf(
+              mapOf("issuingOfficer" to "someone",
+                "dateTimeOfIssue" to "2023-06-08T14:25:00",)
+            ),
+            "dateTimeOfFirstHearing" to "2023-06-08T14:25:00",
+            "outcomes" to arrayListOf(
+            mapOf(
+              "hearing" to mapOf(
+              "id" to 467,
+              "locationId" to 775,
+              "dateTimeOfHearing" to "2023-06-08T14:25:00",
+              "oicHearingType" to "INAD_ADULT",
+              "outcome" to mapOf(
+              "id" to 534,
+              "adjudicator" to "James Warburton",
+              "code" to "COMPLETE",
+              "plea" to "GUILTY"
+              ),
+              "agencyId" to "MDI"
+              ),
+              "outcome" to mapOf(
+              "outcome" to mapOf(
+              "id" to 733,
+              "code" to "CHARGE_PROVED",
+              "canRemove" to true
+              )
+              )
+            )
+            ),
+            "punishments" to arrayListOf(
+              mapOf(
+              "id" to 241,
+              "type" to "PRIVILEGE",
+              "privilegeType" to "TV",
+              "schedule" to mapOf(
+              "days" to 7,
+              "duration" to 7,
+              "measurement" to "DAYS",
+              "startDate" to "2023-06-09",
+              "endDate" to "2023-06-16"
+              ),
+              "canRemove" to true,
+              "canEdit" to true,
+              "rehabilitativeActivities" to mapOf(
+                "id" to 241,
+                "details" to "Some info",
+                "monitor" to "yes",
+                "endDate" to "2023-06-09",
+                "totalSessions" to 16,
+                "completed" to true,
+              )
+              ),
+            mapOf(
+              "id" to 240,
+              "type" to "DAMAGES_OWED",
+              "schedule" to mapOf(
+              "days" to 0,
+              "duration" to 0,
+              "measurement" to "DAYS"
+              ),
+              "damagesOwedAmount" to 20,
+              "canRemove" to true,
+              "canEdit" to true,
+              "rehabilitativeActivities" to emptyList<String>()
+            )
+            ),
+            "punishmentComments" to emptyList<String>(),
+            "outcomeEnteredInNomis" to false,
+            "originatingAgencyId" to "MDI",
+            "linkedChargeNumbers" to emptyList<String>(),
+            "canActionFromHistory" to false
+          )
+        val renderedStyleTemplate = templateRenderService.renderTemplate("hmpps-manage-adjudications-api", testServiceData)
         renderedStyleTemplate.shouldNotBeNull()
         renderedStyleTemplate.shouldContain("<style>")
         renderedStyleTemplate.shouldContain("</style>")
