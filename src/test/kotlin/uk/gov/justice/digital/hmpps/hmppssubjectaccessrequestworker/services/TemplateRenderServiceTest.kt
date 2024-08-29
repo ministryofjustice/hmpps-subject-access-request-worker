da41ca9b00ce610bf4db5e83623f306586098b3d
Anna Horstmann <67278233+HorstmannA@users.noreply.github.com>
2024-08-29 11:21:16 +0100

    Widen keyworker ID field. (#147)
    * Widen keyworker ID field.
    
    * Add width to test.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/TemplateRenderServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/TemplateRenderServiceTest.kt
@@ -95,7 +95,7 @@ class TemplateRenderServiceTest : DescribeSpec(
         renderedStyleTemplate.shouldNotBeNull()
         renderedStyleTemplate.shouldContain("<style>")
         renderedStyleTemplate.shouldContain("</style>")
-        renderedStyleTemplate.shouldContain("<td>Offender Keyworker ID</td><td>12912</td>")
+        renderedStyleTemplate.shouldContain("<td class=\"data-column-40\">Offender Keyworker ID</td><td>12912</td>")
         renderedStyleTemplate.shouldContain("<td>Allocation reason</td><td>MANUAL</td>")
         renderedStyleTemplate.shouldContain("<td>Creation date</td><td>03 December 2019, 11:00:58 am</td>")
       }
