3589dfeebf94c7e06f605d5a9d50ba83fda12235
HorstmannA <anna.horstmann@madetech.com>
2024-08-29 11:10:17 +0100

    Tabulate some activities data.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/GeneratePdfServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/GeneratePdfServiceTest.kt
@@ -643,6 +643,111 @@ class GeneratePdfServiceTest(
         Assertions.assertThat(text).contains("Use of force")
       }
 
+      it("renders a template given an activities template") {
+        val templateRenderService = TemplateRenderService()
+        val testServiceData: ArrayList<Any> = arrayListOf(
+          mapOf(
+            "prisonerNumber" to "A4743DZ",
+            "fromDate" to "1970-01-01",
+            "toDate" to "2000-01-01",
+            "allocations" to arrayListOf(
+              mapOf(
+                "allocationId" to 16,
+                "prisonCode" to "LEI",
+                "prisonerStatus" to "ENDED",
+                "startDate" to "2023-07-21",
+                "endDate" to "2023-07-21",
+                "activityId" to 3,
+                "activitySummary" to "QAtestingKitchenActivity",
+                "payBand" to "Pay band 5",
+                "createdDate" to "2023-07-20",
+              ),
+              mapOf(
+                "allocationId" to 10,
+                "prisonCode" to "LEI",
+                "prisonerStatus" to "NEW",
+                "startDate" to "2023-07-21",
+                "endDate" to "2023-07-21",
+                "activityId" to 4,
+                "activitySummary" to "QAtestingKitchenActivity",
+                "payBand" to "Pay band 5",
+                "createdDate" to "2023-07-20",
+              ),
+            ),
+            "attendanceSummary" to arrayListOf(
+              mapOf(
+                "attendanceReasonCode" to "ATTENDED",
+                "count" to 12,
+              ),
+              mapOf(
+                "attendanceReasonCode" to "CANCELLED",
+                "count" to 8,
+              ),
+            ),
+            "waitingListApplications" to arrayListOf(
+              mapOf(
+                "waitingListId" to 1,
+                "prisonCode" to "LEI",
+                "activitySummary" to "Summary",
+                "applicationDate" to "2023-08-11",
+                "originator" to "Prison staff",
+                "status" to "APPROVED",
+                "statusDate" to "2022-11-12",
+                "comments" to null,
+                "createdDate" to "2023-08-10",
+              ),
+              mapOf(
+                "waitingListId" to 10,
+                "prisonCode" to "LEI",
+                "activitySummary" to "Summary",
+                "applicationDate" to "2024-08-11",
+                "originator" to "Prison staff",
+                "status" to "APPROVED",
+                "statusDate" to null,
+                "comments" to null,
+                "createdDate" to "2023-08-10",
+              ),
+            ),
+            "appointments" to arrayListOf(
+              mapOf(
+                "appointmentId" to 18305,
+                "prisonCode" to "LEI",
+                "categoryCode" to "CAREER",
+                "startDate" to "2023-08-11",
+                "startTime" to "10:00",
+                "endTime" to "11:00",
+                "extraInformation" to "",
+                "attended" to "Unmarked",
+                "createdDate" to "2023-08-10",
+              ),
+              mapOf(
+                "appointmentId" to 16340,
+                "prisonCode" to "LEI",
+                "categoryCode" to "CAREER",
+                "startDate" to "2023-08-11",
+                "startTime" to "10:00",
+                "endTime" to "11:00",
+                "extraInformation" to "",
+                "attended" to "Unmarked",
+                "createdDate" to "2023-08-10",
+              ),
+            ),
+          ),
+        )
+
+        val testResponseObject = listOf(DpsService(name = "hmpps-activities-management-api", content = testServiceData))
+        val writer = PdfWriter(FileOutputStream("dummy-activities-template.pdf"))
+        val mockPdfDocument = PdfDocument(writer)
+        val mockDocument = Document(mockPdfDocument)
+        generatePdfService.addData(mockPdfDocument, mockDocument, testResponseObject)
+        mockDocument.close()
+        val reader = PdfDocument(PdfReader("dummy-activities-template.pdf"))
+        val page = reader.getPage(2)
+        val text = PdfTextExtractor.getTextFromPage(page)
+
+        Assertions.assertThat(text).contains("Activities")
+      }
+
       it("converts data to YAML format in the event of no template") {
         val testInput = mapOf(
           "testDateText" to "Test",
