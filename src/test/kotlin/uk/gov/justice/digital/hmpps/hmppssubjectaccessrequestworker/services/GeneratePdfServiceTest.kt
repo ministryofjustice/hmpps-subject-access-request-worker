409677dc092157da965ac3da4b162b4cf3236baa
AprilMD <95350189+AprilMD@users.noreply.github.com>
2024-09-06 10:56:36 +0100

    Create accredited programmes template (#161)
    * Create template for accredited programmes service
    
    * Add index to list elements in template

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/GeneratePdfServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/GeneratePdfServiceTest.kt
@@ -923,7 +923,6 @@ class GeneratePdfServiceTest(
       }
 
       it("renders for Use of Force Service") {
-
         val testServiceData: ArrayList<Any> = arrayListOf(
           mapOf(
             "id" to 190,
@@ -1202,8 +1201,84 @@ class GeneratePdfServiceTest(
         Assertions.assertThat(text).contains("Incentives")
       }
 
+      it("renders for Accredited Programmes Service") {
+        val testServiceData: Map<Any, Any> = mapOf(
+          "referrals" to arrayListOf(
+            mapOf(
+              "prisonerNumber" to "A8610DY",
+              "oasysConfirmed" to true,
+              "statusCode" to "DESELECTED",
+              "hasReviewedProgrammeHistory" to true,
+              "additionalInformation" to "test",
+              "submittedOn" to "2024-03-12T14:23:12.328775",
+              "referrerUsername" to "AELANGOVAN_ADM",
+              "courseName" to "Becoming New Me Plus",
+              "audience" to "Sexual offence",
+              "courseOrganisation" to "WTI",
+            ),
+            mapOf(
+              "prisonerNumber" to "A8610DY",
+              "oasysConfirmed" to false,
+              "statusCode" to "REFERRAL_STARTED",
+              "hasReviewedProgrammeHistory" to false,
+              "additionalInformation" to null,
+              "submittedOn" to null,
+              "referrerUsername" to "SMCALLISTER_GEN",
+              "courseName" to "Becoming New Me Plus",
+              "audience" to "Intimate partner violence offence",
+              "courseOrganisation" to "AYI",
+            ),
+          ),
+          "courseParticipation" to arrayListOf(
+            mapOf(
+              "prisonerNumber" to "A8610DY",
+              "yearStarted" to null,
+              "source" to null,
+              "type" to "CUSTODY",
+              "outcomeStatus" to "COMPLETE",
+              "yearCompleted" to 2020,
+              "location" to null,
+              "detail" to null,
+              "courseName" to "Kaizen",
+              "createdByUser" to "ACOOMER_GEN",
+              "createdDateTime" to "2024-07-12T14:57:42.431163",
+              "updatedByUser" to "ACOOMER_GEN",
+              "updatedDateTime" to "2024-07-12T14:58:38.597915",
+            ),
+            mapOf(
+              "prisonerNumber" to "A8610DY",
+              "yearStarted" to 2002,
+              "source" to "Example",
+              "type" to "COMMUNITY",
+              "outcomeStatus" to "COMPLETE",
+              "yearCompleted" to 2004,
+              "location" to "Example",
+              "detail" to "Example",
+              "courseName" to "Enhanced Thinking Skills",
+              "createdByUser" to "AELANGOVAN_ADM",
+              "createdDateTime" to "2024-07-12T14:57:42.431163",
+              "updatedByUser" to "AELANGOVAN_ADM",
+              "updatedDateTime" to "2024-07-12T14:58:38.597915",
+            ),
+          ),
+        )
+        val testResponseObject = listOf(DpsService(name = "hmpps-accredited-programmes-api", content = testServiceData))
+        val writer = PdfWriter(FileOutputStream("dummy-accredited-programmes-template.pdf"))
+        val mockPdfDocument = PdfDocument(writer)
+        val mockDocument = Document(mockPdfDocument)
+
+        generatePdfService.addData(mockPdfDocument, mockDocument, testResponseObject)
+
+        mockDocument.close()
+        val reader = PdfDocument(PdfReader("dummy-accredited-programmes-template.pdf"))
+        val page = reader.getPage(2)
+        val text = PdfTextExtractor.getTextFromPage(page)
+
+        Assertions.assertThat(text).contains("Accredited programmes")
+        Assertions.assertThat(text).contains("Referral")
+      }
+
       it("renders a template given an activities template") {
-        val templateRenderService = TemplateRenderService()
         val testServiceData: ArrayList<Any> = arrayListOf(
           mapOf(
             "prisonerNumber" to "A4743DZ",
