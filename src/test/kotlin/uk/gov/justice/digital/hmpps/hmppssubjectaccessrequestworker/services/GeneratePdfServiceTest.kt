61884e7d0bd67ead70e3e66cd754bb775deb739e
April Dawson <aprilmdawson@hotmail.com>
2024-09-13 17:08:31 +0100

    WIP create categorisation template

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/GeneratePdfServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppssubjectaccessrequestworker/services/GeneratePdfServiceTest.kt
@@ -2063,6 +2063,131 @@ class GeneratePdfServiceTest(
         Assertions.assertThat(text).contains("Non-associations")
       }
 
+      it("renders for Categorisation Service") {
+        val testServiceData: Map<Any, Any> = mapOf(
+          "crn" to "X718253",
+          "referral" to arrayListOf(
+            mapOf(
+              "categorisationTool" to mapOf(
+                "catForm" to mapOf(
+                  "form_response" to mapOf(
+                    "ratings" to mapOf(
+                      "escapeRating" to mapOf(
+                        "escapeCatB" to "Yes",
+                        "escapeCatBText" to "Test",
+                        "escapeOtherEvidence" to "Yes",
+                        "escapeOtherEvidenceText" to "Yes"
+                      ),
+                      "extremismRating" to mapOf(
+                        "previousTerrorismOffences" to "Yes",
+                        "previousTerrorismOffencesText" to "Testing - talking about bombs"
+                      ),
+                      "furtherCharges" to mapOf(
+                        "furtherCharges" to "Yes",
+                        "furtherChargesCatB" to "Yes",
+                        "furtherChargesText" to "hjhkjhkjhkj"
+                      ),
+                      "violenceRating" to mapOf(
+                        "seriousThreat" to "Yes",
+                        "seriousThreatText" to "jlkjljkl",
+                        "highRiskOfViolence" to "Yes",
+                        "highRiskOfViolenceText" to "jhkjkkj"
+                      ),
+                      "offendingHistory" to mapOf(
+                        "previousConvictions" to "No"
+                      ),
+                      "securityInput" to mapOf(
+                        "securityInputNeeded" to "Yes",
+                        "securityInputNeededText" to "Test"
+                      ),
+                      "securityBack" to mapOf(
+                        "catB" to "Yes"
+                      ),
+                      "decision" to mapOf(
+                        "category" to "Test"
+                      )
+                    )
+                  ),
+                  "booking_id" to "832899"
+                )
+              )
+
+              "accessibility_needs" to "SAR test 9 - Does Sadie have any other mobility, disability or accessibility needs? (optional)\r\n - None",
+              "additional_needs_information" to "SAR test 10 - Additional information about Sadie’s needs (optional) - None",
+              "when_unavailable" to "SAR test 11 - Provide details of when Sadie will not be able to attend sessions - Weekday mornings",
+              "end_requested_comments" to "",
+              "appointment" to arrayListOf(
+                mapOf(
+                  "session_summary" to "SAR Test 22 - What did you do in the session?",
+                  "session_response" to "SAR Test 23 - How did Sadie Borer respond to the session?",
+                  "session_concerns" to "SAR Test 25 - Yes, something concerned me about Sadie Borer",
+                  "late_reason" to "SAR Test 21 - Add how late they were and anything you know about the reason.",
+                  "future_session_plan" to "SAR Test 26 - Add anything you have planned for the next session (optional)",
+                ),
+                mapOf(
+                  "session_summary" to "SAR 27 - What did you do in the session?",
+                  "session_response" to "SAR 28 - How did Sadie Borer respond to the session?",
+                  "session_concerns" to "SAR 30 - Yes, something concerned me about Sadie Borer",
+                  "late_reason" to "",
+                  "future_session_plan" to "SAR 31 - Add anything you have planned for the next session (optional)",
+                ),
+              ),
+              "action_plan_activity" to arrayListOf(
+                mapOf(
+                  "description" to arrayListOf(
+                    "SAR Test 19 - Please write the details of the activity here.",
+                    "SAR Test 20 - Activity 2 - Please write the details of the activity here.",
+                  ),
+                ),
+                mapOf(
+                  "description" to arrayListOf(
+                    "example",
+                  ),
+                ),
+              ),
+              "end_of_service_report" to mapOf(
+                "end_of_service_outcomes" to arrayListOf(
+                  mapOf(
+                    "progression_comments" to "SAR Test 32 - Describe their progress on this outcome.",
+                    "additional_task_comments" to "SAR Test 33 - Enter if anything else needs to be done (optional)",
+                  ),
+                  mapOf(
+                    "progression_comments" to "test.",
+                    "additional_task_comments" to "test",
+                  ),
+                ),
+              ),
+            ),
+            mapOf(
+              "referral_number" to "FY7705FI",
+              "accessibility_needs" to "mobility",
+              "additional_needs_information" to "",
+              "when_unavailable" to "Fridays",
+              "end_requested_comments" to "",
+              "appointment" to emptyList<Any>(),
+              "action_plan_activity" to emptyList<Any>(),
+              "end_of_service_report" to null,
+            ),
+          ),
+        )
+
+
+
+        val testResponseObject = listOf(DpsService(name = "hmpps-offender-categorisation-api", content = testServiceData))
+        val writer = PdfWriter(FileOutputStream("dummy-categorisation-api-template.pdf"))
+        val mockPdfDocument = PdfDocument(writer)
+        val mockDocument = Document(mockPdfDocument)
+
+        generatePdfService.addData(mockPdfDocument, mockDocument, testResponseObject)
+
+        mockDocument.close()
+        val reader = PdfDocument(PdfReader("dummy-categorisation-api-template.pdf"))
+        val page = reader.getPage(2)
+        val text = PdfTextExtractor.getTextFromPage(page)
+
+        Assertions.assertThat(text).contains("Prisoner categorisation")
+      }
+
       it("converts data to YAML format in the event of no template") {
         val testInput = mapOf(
           "testDateText" to "Test",
