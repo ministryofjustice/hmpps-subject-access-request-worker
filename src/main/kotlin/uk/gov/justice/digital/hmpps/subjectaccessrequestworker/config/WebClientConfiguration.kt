24da7aeb802390a58e1aa0d89bab60d5017b7bc5
DaveLlewellynMoJ <165893357+DaveLlewellynMoJ@users.noreply.github.com>
2024-10-14 11:01:05 +0100

    Add client retries. (#189)
    Added retry to SAR api request for get unclaimed jobs

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/config/WebClientConfiguration.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.subjectaccessrequestworker.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import java.time.Duration
+
+@Configuration
+class WebClientConfiguration(
+  @Value("\${web-client.configuration.max-retries:0}") val maxRetries: Long,
+  @Value("\${web-client.configuration.back-off:PT10S}") val backOff: String,
+) {
+
+  private var backOffDuration: Duration = Duration.parse(backOff)
+
+  fun getBackoffDuration() = backOffDuration
+
+  override fun toString(): String {
+    return "WebClientConfiguration(maxRetries=$maxRetries, backOff=$backOff)"
+  }
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/gateways/SubjectAccessRequestGateway.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/gateways/SubjectAccessRequestGateway.kt
@@ -1,16 +1,32 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestworker.gateways
 
-import org.springframework.beans.factory.annotation.Autowired
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatusCode
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientRequestException
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.util.retry.Retry
+import uk.gov.justice.digital.hmpps.subjectaccessrequestworker.config.WebClientConfiguration
 import uk.gov.justice.digital.hmpps.subjectaccessrequestworker.models.SubjectAccessRequest
+import java.time.Duration
 
 @Service
-class SubjectAccessRequestGateway(@Autowired val hmppsAuthGateway: HmppsAuthGateway) {
+class SubjectAccessRequestGateway(
+  val hmppsAuthGateway: HmppsAuthGateway,
+  val webClientConfig: WebClientConfiguration,
+) {
+
+  private val backoff: Duration = webClientConfig.getBackoffDuration()
+  private val maxRetries: Long = webClientConfig.maxRetries
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
 
   fun getClient(url: String): WebClient {
-    return WebClient.create(url)
+    return WebClient.create(url) // TODO what other config does this need?
   }
   fun getClientTokenFromHmppsAuth(): String {
     return hmppsAuthGateway.getClientToken()
@@ -18,12 +34,35 @@ class SubjectAccessRequestGateway(@Autowired val hmppsAuthGateway: HmppsAuthGate
 
   fun getUnclaimed(client: WebClient): Array<SubjectAccessRequest>? {
     val token = this.getClientTokenFromHmppsAuth()
-    return client.get().uri("/api/subjectAccessRequests?unclaimed=true").header("Authorization", "Bearer $token").retrieve().bodyToMono(Array<SubjectAccessRequest>::class.java).block()
+
+    return client
+      .get()
+      .uri("/api/subjectAccessRequests?unclaimed=true")
+      .header("Authorization", "Bearer $token")
+      .retrieve()
+      .bodyToMono(Array<SubjectAccessRequest>::class.java)
+      .retryWhen(
+        Retry
+          .backoff(maxRetries, backoff)
+          .filter { error ->
+            isRetryableError(error).also {
+              log.info("request failed with error: ${error.message} will attempt retry? $it, back-off: $backoff")
+            }
+          }
+          .onRetryExhaustedThrow { _, signal ->
+            log.info("request retry attempts (${signal.totalRetriesInARow()}) exhausted, cause: ${signal.failure().message} ")
+            signal.failure()
+          },
+      ).block()
   }
 
   fun claim(client: WebClient, chosenSAR: SubjectAccessRequest): HttpStatusCode? {
     val token = this.getClientTokenFromHmppsAuth()
-    val patchResponse = client.patch().uri("/api/subjectAccessRequests/" + chosenSAR.id.toString() + "/claim").header("Authorization", "Bearer $token").retrieve()
+    val patchResponse = client
+      .patch()
+      .uri("/api/subjectAccessRequests/${chosenSAR.id}/claim")
+      .header("Authorization", "Bearer $token")
+      .retrieve()
     return patchResponse.toBodilessEntity().block()?.statusCode
   }
 
@@ -31,5 +70,15 @@ class SubjectAccessRequestGateway(@Autowired val hmppsAuthGateway: HmppsAuthGate
     val token = this.getClientTokenFromHmppsAuth()
     val patchResponse = client.patch().uri("/api/subjectAccessRequests/" + chosenSAR.id.toString() + "/complete").header("Authorization", "Bearer $token").retrieve()
     return patchResponse.toBodilessEntity().block()?.statusCode
+  }
+
+  private fun isRetryableError(t: Throwable): Boolean {
+    return when (t) {
+      is WebClientResponseException -> {
+        return t.statusCode.is5xxServerError
+      }
+      is WebClientRequestException -> true
+      else -> false
+    }
   }
 }
