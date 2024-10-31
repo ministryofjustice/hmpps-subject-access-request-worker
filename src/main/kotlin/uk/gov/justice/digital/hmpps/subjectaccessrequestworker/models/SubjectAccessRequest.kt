e6f07312fc681a68d930829b968fd6a8b1b1fa33
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2024-10-31 10:48:25 +0000

    HAAR-3010: Remove hibernate caching from getting style templates as service has no database - if needed should use spring caching

--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/models/SubjectAccessRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/models/SubjectAccessRequest.kt
@@ -1,9 +1,5 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestworker.models
 
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.Id
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
@@ -13,11 +9,8 @@ enum class Status {
   Completed,
 }
 
-@Entity
 data class SubjectAccessRequest(
-  @Id
   val id: UUID = UUID.randomUUID(),
-  @Enumerated(EnumType.STRING)
   val status: Status = Status.Pending,
   val dateFrom: LocalDate? = null,
   val dateTo: LocalDate? = null,
