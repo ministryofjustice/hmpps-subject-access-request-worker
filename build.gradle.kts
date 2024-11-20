56404aae81d72d09e72481b4c9aef8a3c450226e
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2024-11-20 16:15:10 +0000

    HAAR-3341: ⬆️ update to spring-boot-plugin-6.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9"
   kotlin("plugin.spring") version "2.0.21"
 }
 
@@ -13,15 +13,15 @@ dependencies {
   implementation("org.json:json:20240303")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.9.0")
-  implementation("com.itextpdf:itext7-core:8.0.5")
-  implementation("com.itextpdf:html2pdf:5.0.5")
+  implementation("com.itextpdf:itext7-core:9.0.0")
+  implementation("com.itextpdf:html2pdf:6.0.0")
   implementation("org.springframework.boot:spring-boot-starter-mustache")
   implementation("com.github.spullara.mustache.java:compiler:0.9.14")
   implementation("com.github.jknack:handlebars:4.4.0")
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.3")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.17.0")
-  implementation("io.sentry:sentry-logback:7.17.0")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.1")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:7.18.0")
+  implementation("io.sentry:sentry-logback:7.18.0")
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.8")
 
   runtimeOnly("org.flywaydb:flyway-core")
