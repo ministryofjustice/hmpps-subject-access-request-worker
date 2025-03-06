d8ab993a8d869f32a4b7f6940e0dc51e516eeee7
Inderjit Virdi <inderjit.virdi@digital.justice.gov.uk>
2025-03-06 16:34:33 +0000

    HAAR-3711: Update to use latest version of dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.3"
   kotlin("plugin.spring") version "2.1.10"
 }
 
@@ -17,16 +17,16 @@ dependencies {
   implementation("org.json:json:20250107")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.1")
-  implementation("com.itextpdf:itext7-core:9.0.0")
-  implementation("com.itextpdf:html2pdf:6.0.0")
+  implementation("com.itextpdf:itext7-core:9.1.0")
+  implementation("com.itextpdf:html2pdf:6.1.0")
   implementation("org.springframework.boot:spring-boot-starter-mustache")
   implementation("com.github.spullara.mustache.java:compiler:0.9.14")
   implementation("com.github.jknack:handlebars:4.4.0")
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.2.0")
-  implementation("io.sentry:sentry-logback:8.2.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.2.1")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.3")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.3.0")
+  implementation("io.sentry:sentry-logback:8.3.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.3.1")
   implementation("commons-io:commons-io:2.18.0")
 
   runtimeOnly("org.flywaydb:flyway-core")
@@ -34,8 +34,8 @@ dependencies {
   runtimeOnly("org.postgresql:postgresql:42.7.5")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.2.1")
-  testImplementation("org.wiremock:wiremock-standalone:3.12.0")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.3.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.12.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("com.google.code.gson:gson:2.12.1")
 }
