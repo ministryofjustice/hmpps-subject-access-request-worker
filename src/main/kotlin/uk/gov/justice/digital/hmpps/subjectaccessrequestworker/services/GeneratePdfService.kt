e4352708904f60a0b5171a13c1eebfefd907595b
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2025-04-03 11:56:58 +0100

    removed test debug log

--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/services/GeneratePdfService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/services/GeneratePdfService.kt
@@ -305,10 +305,8 @@ class GeneratePdfService(
     document.add(Paragraph(getSubjectIdLine(nomisId, ndeliusCaseReferenceId)).setTextAlignment(TextAlignment.CENTER))
     document.add(Paragraph("SAR Case Reference Number: $sarCaseReferenceNumber").setTextAlignment(TextAlignment.CENTER))
     document.add(Paragraph(getReportDateRangeLine(dateFrom, dateTo)).setTextAlignment(TextAlignment.CENTER))
-    val reportGenerationDate = dateService.reportGenerationDate()
-    log.info("report generation date: $reportGenerationDate")
     document.add(
-      Paragraph("Report generation date: $reportGenerationDate").setTextAlignment(
+      Paragraph("Report generation date: ${dateService.reportGenerationDate()}").setTextAlignment(
         TextAlignment.CENTER,
       ),
     )
