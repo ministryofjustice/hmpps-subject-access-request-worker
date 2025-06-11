c2890e6e54c1cd12484b83c3a21de004f568a35a
Dave Llewellyn <david.llewellyn@digital.justice.gov.uk>
2025-06-11 16:32:51 +0100

    added more int test for backlog processor

--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/integration/BacklogRequestProcessorIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/integration/BacklogRequestProcessorIntTest.kt
@@ -5,6 +5,8 @@ import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.findByIdOrNull
 import uk.gov.justice.digital.hmpps.subjectaccessrequestworker.client.DynamicServicesClient
@@ -44,21 +46,21 @@ class BacklogRequestProcessorIntTest : IntegrationTestBase() {
 
   private val serviceConfigurations = listOf(
     ServiceConfiguration(
-      serviceName = "service-001",
+      serviceName = "service-1",
       label = "Service 1",
       url = "http://localhost:${htmlRendererApi.port()}",
       order = 1,
       enabled = true,
     ),
     ServiceConfiguration(
-      serviceName = "service-002",
+      serviceName = "service-2",
       label = "Service 2",
       url = "http://localhost:${htmlRendererApi.port()}",
       order = 2,
       enabled = true,
     ),
     ServiceConfiguration(
-      serviceName = "service-003",
+      serviceName = "service-3",
       label = "Service 3",
       url = "http://localhost:${htmlRendererApi.port()}",
       order = 3,
@@ -73,27 +75,170 @@ class BacklogRequestProcessorIntTest : IntegrationTestBase() {
     serviceConfigurationService.saveAll(serviceConfigurations)
   }
 
-  @Test
-  fun `backlog request is successfully processed and marked complete`() {
+  @ParameterizedTest
+  @CsvSource(
+    value = [
+      "true   | 'Completed backlog request should show data held TRUE when services holds data on subject'",
+      "false  | 'Completed backlog request should show data held FALSE when services does not hold data on subject'",
+    ],
+    delimiter = '|',
+  )
+  fun `backlog request is successfully processed when subject data is held`(
+    dataIsHeld: Boolean,
+    description: String,
+  ) {
     val start = LocalDateTime.now()
     val backlogRequest = createBacklogRequest()
     assertThat(backlogRequest).isNotNull
 
     hmppsAuth.stubGrantToken()
-    serviceConfigurations.forEach { stubRendererSubjectDataHeldResponse(it.serviceName, true) }
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-1"), dataIsHeld)
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-2"), dataIsHeld)
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-3"), dataIsHeld)
+
+    await()
+      .atMost(5, TimeUnit.SECONDS)
+      .until { requestIsComplete(backlogRequest!!.id) }
+
+    val result = assertBacklogRequestEqualsExpected(
+      backlogRequestId = backlogRequest!!.id,
+      createdAfter = start,
+      expectedDataHeld = dataIsHeld,
+      expectedStatus = COMPLETE,
+      expectedClaimDateTimeAfter = start,
+    )
+
+    assertThat(result.serviceSummary).hasSize(serviceConfigurations.size)
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-1",
+      expectedOrder = 1,
+      expectedDataHeld = dataIsHeld,
+      expectedStatus = COMPLETE,
+    )
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-2",
+      expectedOrder = 2,
+      expectedDataHeld = dataIsHeld,
+      expectedStatus = COMPLETE,
+    )
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-3",
+      expectedOrder = 3,
+      expectedDataHeld = dataIsHeld,
+      expectedStatus = COMPLETE,
+    )
+
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-1"))
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-2"))
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-3"))
+  }
+
+  @Test
+  fun `backlog request is successful after retrying failed renderer request`() {
+    val start = LocalDateTime.now()
+    val backlogRequest = createBacklogRequest()
+    assertThat(backlogRequest).isNotNull
+
+    hmppsAuth.stubGrantToken()
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-1"), true)
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-2"), true)
+    stubRendererSubjectDataHeldFailsOnFirstAttempt(createSubjectDataHeldRequest("service-3"), true)
 
     await()
       .atMost(3, TimeUnit.SECONDS)
       .until { requestIsComplete(backlogRequest!!.id) }
 
-    val result = assertBacklogRequestIsComplete(
+    val result = assertBacklogRequestEqualsExpected(
       backlogRequestId = backlogRequest!!.id,
       createdAfter = start,
       expectedDataHeld = true,
       expectedStatus = COMPLETE,
+      expectedClaimDateTimeAfter = start,
     )
 
-    assertBacklogRequestContainsExpectedServiceSummaries(result)
+    assertThat(result.serviceSummary).hasSize(serviceConfigurations.size)
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-1",
+      expectedOrder = 1,
+      expectedDataHeld = true,
+      expectedStatus = COMPLETE,
+    )
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-2",
+      expectedOrder = 2,
+      expectedDataHeld = true,
+      expectedStatus = COMPLETE,
+    )
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-3",
+      expectedOrder = 3,
+      expectedDataHeld = true,
+      expectedStatus = COMPLETE,
+    )
+
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-1"))
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-2"))
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(2, createSubjectDataHeldRequest("service-3"))
+  }
+
+  @Test
+  fun `backlog request is successful with data held TRUE when some services do not hold subject data`() {
+    val start = LocalDateTime.now()
+    val backlogRequest = createBacklogRequest()
+    assertThat(backlogRequest).isNotNull
+
+    hmppsAuth.stubGrantToken()
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-1"), false)
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-2"), false)
+    stubRendererSubjectDataHeldResponse(createSubjectDataHeldRequest("service-3"), true)
+
+    await()
+      .atMost(3, TimeUnit.SECONDS)
+      .until { requestIsComplete(backlogRequest!!.id) }
+
+    val result = assertBacklogRequestEqualsExpected(
+      backlogRequestId = backlogRequest!!.id,
+      createdAfter = start,
+      expectedDataHeld = true,
+      expectedStatus = COMPLETE,
+      expectedClaimDateTimeAfter = start,
+    )
+
+    val serviceConfigurations = serviceConfigurationService.getAllServiceConfigurations()
+    assertThat(result.serviceSummary).hasSize(serviceConfigurations.size)
+
+    assertThat(result.serviceSummary).hasSize(serviceConfigurations.size)
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-1",
+      expectedOrder = 1,
+      expectedDataHeld = false,
+      expectedStatus = COMPLETE,
+    )
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-2",
+      expectedOrder = 2,
+      expectedDataHeld = false,
+      expectedStatus = COMPLETE,
+    )
+    assertServiceSummaryExistsWithExpectedValues(
+      backlogRequestId = result.id,
+      serviceName = "service-3",
+      expectedOrder = 3,
+      expectedDataHeld = true,
+      expectedStatus = COMPLETE,
+    )
+
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-1"))
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-2"))
+    htmlRendererApi.verifySubjectDataHeldSummaryCalled(1, createSubjectDataHeldRequest("service-3"))
   }
 
   private fun createBacklogRequest(): BacklogResponseEntity? = webTestClient
@@ -121,16 +266,12 @@ class BacklogRequestProcessorIntTest : IntegrationTestBase() {
 
   private fun DynamicServicesClient.SubjectDataHeldResponse.toJson(): String = objectMapper.writeValueAsString(this)
 
-  private fun stubRendererSubjectDataHeldResponse(serviceName: String, dataHeld: Boolean) {
+  private fun stubRendererSubjectDataHeldResponse(
+    expectedSubjectDataHeldRequest: DynamicServicesClient.SubjectDataHeldRequest,
+    dataHeld: Boolean,
+  ) {
     htmlRendererApi.stubSubjectDataHeldResponse(
-      subjectDataHeldRequest = DynamicServicesClient.SubjectDataHeldRequest(
-        nomisId = testNomisId,
-        ndeliusId = null,
-        serviceName = serviceName,
-        serviceUrl = "http://localhost:${htmlRendererApi.port()}",
-        dateFrom = dateFrom,
-        dateTo = dateTo,
-      ),
+      subjectDataHeldRequest = expectedSubjectDataHeldRequest,
       responseDefinition = ResponseDefinitionBuilder
         .responseDefinition()
         .withHeader("Content-Type", "application/json")
@@ -139,17 +280,42 @@ class BacklogRequestProcessorIntTest : IntegrationTestBase() {
             nomisId = testNomisId,
             ndeliusId = testNdeliusId,
             dataHeld = dataHeld,
-            serviceName = serviceName,
+            serviceName = expectedSubjectDataHeldRequest.serviceName,
           ).toJson(),
         ).withStatus(200),
     )
   }
 
-  private fun assertBacklogRequestIsComplete(
+  private fun stubRendererSubjectDataHeldFailsOnFirstAttempt(
+    expectedSubjectDataHeldRequest: DynamicServicesClient.SubjectDataHeldRequest,
+    dataHeld: Boolean,
+  ) {
+    htmlRendererApi.stubSubjectDataHeldResponseRetryAfterFailure(
+      subjectDataHeldRequest = expectedSubjectDataHeldRequest,
+      responseOne = ResponseDefinitionBuilder
+        .responseDefinition()
+        .withHeader("Content-Type", "application/json")
+        .withStatus(500),
+      responseTwo = ResponseDefinitionBuilder
+        .responseDefinition()
+        .withHeader("Content-Type", "application/json")
+        .withBody(
+          DynamicServicesClient.SubjectDataHeldResponse(
+            nomisId = testNomisId,
+            ndeliusId = testNdeliusId,
+            dataHeld = dataHeld,
+            serviceName = expectedSubjectDataHeldRequest.serviceName,
+          ).toJson(),
+        ).withStatus(200),
+    )
+  }
+
+  private fun assertBacklogRequestEqualsExpected(
     backlogRequestId: UUID,
     createdAfter: LocalDateTime,
     expectedDataHeld: Boolean,
     expectedStatus: BacklogRequestStatus,
+    expectedClaimDateTimeAfter: LocalDateTime?,
   ): BacklogRequest {
     val backlogRequest = backlogRequestRepository.findByIdOrNull(backlogRequestId)
     assertThat(backlogRequest).isNotNull
@@ -157,27 +323,40 @@ class BacklogRequestProcessorIntTest : IntegrationTestBase() {
     assertThat(backlogRequest.completedAt).isNotNull()
     assertThat(backlogRequest.completedAt).isBetween(createdAfter, LocalDateTime.now())
     assertThat(backlogRequest.dataHeld).isEqualTo(expectedDataHeld)
+    expectedClaimDateTimeAfter?.let {
+      assertThat(backlogRequest.claimDateTime).isBetween(it, LocalDateTime.now())
+    }
     return backlogRequest
   }
 
-  private fun assertBacklogRequestContainsExpectedServiceSummaries(backlogRequest: BacklogRequest) {
-    val serviceConfigurations = serviceConfigurationService.getAllServiceConfigurations()
-    assertThat(backlogRequest.serviceSummary).hasSize(serviceConfigurations.size)
+  private fun assertServiceSummaryExistsWithExpectedValues(
+    backlogRequestId: UUID,
+    serviceName: String,
+    expectedOrder: Int,
+    expectedDataHeld: Boolean,
+    expectedStatus: BacklogRequestStatus,
+  ) {
+    val serviceSummary = serviceSummaryRepository.findOneByBacklogRequestIdAndServiceNameAndStatus(
+      backlogRequestId = backlogRequestId,
+      serviceName = serviceName,
+      status = COMPLETE,
+    )
+    assertThat(serviceSummary).isNotNull
 
-    serviceConfigurations.forEach { cfg ->
-      val serviceSummary = serviceSummaryRepository.findOneByBacklogRequestIdAndServiceNameAndStatus(
-        backlogRequestId = backlogRequest.id,
-        serviceName = cfg.serviceName,
-        status = COMPLETE,
-      )
-      assertThat(serviceSummary).isNotNull
-
-      assertThat(serviceSummary!!.serviceName).isEqualTo(cfg.serviceName)
-      assertThat(serviceSummary.serviceOrder).isEqualTo(cfg.order)
-      assertThat(serviceSummary.backlogRequest).isNotNull
-      assertThat(serviceSummary.backlogRequest!!.id).isEqualTo(backlogRequest.id)
-      assertThat(serviceSummary.dataHeld).isTrue()
-      assertThat(serviceSummary.status).isEqualTo(COMPLETE)
-    }
+    assertThat(serviceSummary!!.serviceName).isEqualTo(serviceName)
+    assertThat(serviceSummary.serviceOrder).isEqualTo(expectedOrder)
+    assertThat(serviceSummary.backlogRequest).isNotNull
+    assertThat(serviceSummary.backlogRequest!!.id).isEqualTo(backlogRequestId)
+    assertThat(serviceSummary.dataHeld).isEqualTo(expectedDataHeld)
+    assertThat(serviceSummary.status).isEqualTo(expectedStatus)
   }
+
+  private fun createSubjectDataHeldRequest(serviceName: String) = DynamicServicesClient.SubjectDataHeldRequest(
+    nomisId = testNomisId,
+    ndeliusId = null,
+    serviceName = serviceName,
+    serviceUrl = "http://localhost:${htmlRendererApi.port()}",
+    dateFrom = dateFrom,
+    dateTo = dateTo,
+  )
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/mockservers/HtmlRendererMockServer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/mockservers/HtmlRendererMockServer.kt
@@ -79,10 +79,40 @@ class HtmlRendererMockServer : WireMockServer(8087) {
     )
   }
 
+  fun stubSubjectDataHeldResponseRetryAfterFailure(
+    subjectDataHeldRequest: DynamicServicesClient.SubjectDataHeldRequest,
+    responseOne: ResponseDefinitionBuilder,
+    responseTwo: ResponseDefinitionBuilder,
+  ) {
+    stubFor(
+      post("/subject-access-request/subject-data-held-summary")
+        .withRequestBody(equalToJson(objectMapper.writeValueAsString(subjectDataHeldRequest)))
+        .inScenario("request-1-then-2")
+        .willReturn(responseOne)
+        .willSetStateTo("response-1-done")
+    )
+
+    stubFor(
+      post("/subject-access-request/subject-data-held-summary")
+        .inScenario("request-1-then-2")
+        .withRequestBody(equalToJson(objectMapper.writeValueAsString(subjectDataHeldRequest)))
+        .whenScenarioStateIs("response-1-done")
+        .willReturn(responseTwo),
+    )
+  }
+
   fun verifyRenderCalled(times: Int, expectedBody: HtmlRendererApiClient.HtmlRenderRequest) {
     verify(
       times,
       postRequestedFor(urlPathEqualTo("/subject-access-request/render"))
+        .withRequestBody(equalToJson(objectMapper.writeValueAsString(expectedBody))),
+    )
+  }
+
+  fun verifySubjectDataHeldSummaryCalled(times: Int, expectedBody: DynamicServicesClient.SubjectDataHeldRequest) {
+    verify(
+      times,
+      postRequestedFor(urlPathEqualTo("/subject-access-request/subject-data-held-summary"))
         .withRequestBody(equalToJson(objectMapper.writeValueAsString(expectedBody))),
     )
   }
