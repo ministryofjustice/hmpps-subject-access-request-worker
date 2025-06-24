1e464f9217dd25a3f8319559c1b7ce88b97cd36d
simon.mitchell <simon.mitchell@digital.justice.gov.uk>
2025-06-24 14:11:38 +0100

    HAAR-4059: ⬆️ update to spring-boot-plugin-8.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.2.0"
-  kotlin("plugin.spring") version "2.1.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.0"
+  kotlin("plugin.spring") version "2.2.0"
 }
 
 configurations {
@@ -23,19 +23,19 @@ dependencies {
   implementation("com.github.spullara.mustache.java:compiler:0.9.14")
   implementation("com.github.jknack:handlebars:4.4.0")
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.0")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.13.3")
-  implementation("io.sentry:sentry-logback:8.13.3")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.6")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.1")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.14.0")
+  implementation("io.sentry:sentry-logback:8.14.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.7")
   implementation("commons-io:commons-io:2.19.0")
-  implementation("aws.sdk.kotlin:s3:1.4.103")
+  implementation("aws.sdk.kotlin:s3:1.4.111")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("com.h2database:h2:2.3.232")
   runtimeOnly("org.postgresql:postgresql:42.7.7")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.6")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.4.7")
   testImplementation("org.wiremock:wiremock-standalone:3.13.1")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("com.google.code.gson:gson:2.13.1")
