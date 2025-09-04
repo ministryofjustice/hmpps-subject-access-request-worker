fdfc6656483e8a259f61342101974162486ebf67
indyv-moj <inderjit.virdi@digital.justice.gov.uk>
2025-09-04 16:51:27 +0100

    Fix health check for goterberg component (#408)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/health/HealthPingCheck.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/health/HealthPingCheck.kt
@@ -1,8 +1,12 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestworker.health
 
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.core.publisher.Mono
 import uk.gov.justice.hmpps.kotlin.health.HealthPingCheck
 
 @Component("hmppsAuth")
@@ -24,4 +28,15 @@ class LocationsApiHealthPing(@Qualifier("locationsApiHealthWebClient") webClient
 class NomisMappingsApiHealthPing(@Qualifier("nomisMappingsApiHealthWebClient") webClient: WebClient) : HealthPingCheck(webClient)
 
 @Component("gotenbergApi")
-class GotenbergHealth(@Qualifier("gotenbergWebClient") webClient: WebClient) : HealthPingCheck(webClient)
+class GotenbergHealth(@Qualifier("gotenbergWebClient") private val webClient: WebClient) : HealthIndicator {
+  override fun health(): Health? = webClient.get()
+    .uri("/health")
+    .retrieve()
+    .toEntity(String::class.java)
+    .flatMap { Mono.just(Health.up().withDetail("HttpStatus", it?.statusCode).build()) }
+    .onErrorResume(WebClientResponseException::class.java) {
+      Mono.just(Health.down(it).withDetail("body", it.responseBodyAsString).withDetail("HttpStatus", it.statusCode).build())
+    }
+    .onErrorResume(Exception::class.java) { Mono.just(Health.down(it).build()) }
+    .block() ?: Health.down().withDetail("HttpStatus", "No response returned from health").build()
+}
