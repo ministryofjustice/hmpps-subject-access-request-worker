2f82003c36d79c1690dfa43ab26aacbdbbe58228
Simon Mitchell <51327941+simon-mitchell@users.noreply.github.com>
2025-11-25 15:20:39 +0000

    HAAR-4693: ⬆️ update to spring-boot-plugin-9.2.0 (#427)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.2.0"
   kotlin("plugin.spring") version "2.2.21"
 }
 
@@ -20,30 +20,30 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.json:json:20250517")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.10.2")
-  implementation("com.itextpdf:itext7-core:9.3.0")
-  implementation("com.itextpdf:html2pdf:6.2.1")
+  implementation("com.itextpdf:itext7-core:9.4.0")
+  implementation("com.itextpdf:html2pdf:6.3.0")
   implementation("org.springframework.boot:spring-boot-starter-mustache")
   implementation("com.github.spullara.mustache.java:compiler:0.9.14")
   implementation("com.github.jknack:handlebars:4.5.0")
-  implementation("org.apache.commons:commons-lang3:3.19.0")
-  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.0")
-  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.24.0")
-  implementation("io.sentry:sentry-logback:8.24.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.7.0")
-  implementation("commons-io:commons-io:2.20.0")
-  implementation("aws.sdk.kotlin:s3:1.5.68")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+  implementation("org.apache.commons:commons-lang3:3.20.0")
+  implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.20.1")
+  implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.27.1")
+  implementation("io.sentry:sentry-logback:8.27.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.8.2")
+  implementation("commons-io:commons-io:2.21.0")
+  implementation("aws.sdk.kotlin:s3:1.5.90")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.14")
   runtimeOnly("org.flywaydb:flyway-core")
   runtimeOnly("com.h2database:h2:2.4.240")
   runtimeOnly("org.postgresql:postgresql:42.7.8")
   runtimeOnly("org.flywaydb:flyway-database-postgresql")
   implementation("org.apache.commons:commons-csv:1.14.1")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.7.0")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.8.2")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.2")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
   testImplementation("com.opencsv:opencsv:5.12.0")
-  testImplementation("org.testcontainers:testcontainers:2.0.1")
+  testImplementation("org.testcontainers:testcontainers:2.0.2")
   testImplementation("org.testcontainers:junit-jupiter:1.21.3")
 }
 
