ec8b05b97cd043ce1aee8a1aa7df7ba6c3344f59
Inderjit Virdi <inderjit.virdi@digital.justice.gov.uk>
2026-04-28 10:00:13 +0100

    HAAR-5215: Allow for page breaks in reports and update wiremock stubs for inline attachments

--- src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/utils/PdfTestUtil.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/subjectaccessrequestworker/utils/PdfTestUtil.kt
@@ -1,13 +1,18 @@
 package uk.gov.justice.digital.hmpps.subjectaccessrequestworker.utils
 
 import com.itextpdf.kernel.pdf.PdfDocument
+import com.itextpdf.kernel.pdf.PdfName
+import com.itextpdf.kernel.pdf.PdfPage
 import com.itextpdf.kernel.pdf.PdfReader
+import com.itextpdf.kernel.pdf.PdfStream
 import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor
 import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy
+import com.itextpdf.kernel.pdf.xobject.PdfImageXObject
 import org.assertj.core.api.Assertions.assertThat
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.InputStream
+import java.security.MessageDigest
 
 class PdfTestUtil {
 
@@ -50,12 +55,32 @@ class PdfTestUtil {
 
       for (i in 1..actual.numberOfPages) {
         val actualPageN = PdfTextExtractor.getTextFromPage(actual.getPage(i), SimpleTextExtractionStrategy())
+        val actualPageImages = extractImagesFromPage(actual.getPage(i))
         val expectedPageN = PdfTextExtractor.getTextFromPage(expected.getPage(i), SimpleTextExtractionStrategy())
+        val expectedPageImages = extractImagesFromPage(expected.getPage(i))
 
         assertThat(actualPageN)
           .isEqualTo(expectedPageN)
-          .withFailMessage("actual page: $i did not match expected.")
+          .withFailMessage("actual page: $i did not match expected text.")
+        assertThat(actualPageImages).isEqualTo(expectedPageImages)
+          .withFailMessage("actual page: $i did not contain expected images.")
       }
     }
+
+    fun extractImagesFromPage(page: PdfPage): List<PdfImageInfo> = page.resources
+      ?.getResource(PdfName.XObject)?.values()
+      ?.filter { obj -> obj is PdfStream && obj.getAsName(PdfName.Subtype) == PdfName.Image }
+      ?.map { obj ->
+        val image = PdfImageXObject(obj as PdfStream)
+        val hash =
+          MessageDigest.getInstance("SHA-256").digest(image.imageBytes).joinToString("") { "%02x".format(it) }
+        PdfImageInfo(width = image.width, height = image.height, hash = hash)
+      } ?: emptyList()
   }
 }
+
+data class PdfImageInfo(
+  val width: Float,
+  val height: Float,
+  val hash: String,
+)
